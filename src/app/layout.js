6a2645b29e29360d1d6383909346b1990f521fc6
Atik03 <atikshaharia03@gmail.com>
2026-05-01 21:29:15 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,8 +21,9 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme="light"
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="">{children}</body>
     </html>
   );
 }
---
+++ src/app/tiles/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+
+function AllPhoto() {
+  return <div className="">All-photo</div>;
+}
+
+export default AllPhoto;
