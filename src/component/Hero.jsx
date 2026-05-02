daf579af2f87f5c45354182401d181c19c27e66f
Atik03 <atikshaharia03@gmail.com>
2026-05-02 14:22:38 +0600

    Add Hero Section

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,7 +1,32 @@
+import Link from "next/link";
 import React from "react";
 
 function Hero() {
-  return <h1></h1>;
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: "url('/images/hero.png')",
+      }}
+    >
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">TilesBrand</h1>
+          <p className="leading-relaxed mb-5">
+            {" "}
+            Discover premium quality tiles crafted for modern homes and
+            commercial spaces. From elegant ceramic designs to durable flooring
+            solutions, we bring style, strength, and sophistication to every
+            surface.{" "}
+          </p>
+          <Link href={"/tiles"} className="btn btn-primary">
+            Browse Now
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
